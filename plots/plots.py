d947e656d2b6ec13a364715548fd6b3d02c793f2
GonzaloRossin <gonzarossin@gmail.com>
2022-10-31 13:18:26 -0300

    feat: add plot python file

---
+++ plots/plots.py
@@ -0,0 +1,37 @@
+import matplotlib.pylab as plt
+import pandas as pd
+
+def QvsT():
+    df = pd.read_json('QvsTime.json')
+
+    plt.plot(df['time'],df['Q'])
+    plt.legend()
+    plt.xlabel("Tiempo [S] ", fontsize=16)
+    plt.ylabel("Q", fontsize=16)
+    plt.show()
+
+def parcticlesvsT():
+    df = pd.read_json('ParticlesvsTime.json')
+
+    plt.plot(df['time'],df['particles'])
+    plt.legend()
+    plt.xlabel("Tiempo [S] ", fontsize=16)
+    plt.ylabel("Partículas", fontsize=16)
+    plt.show()
+
+'''''df = pd.read_json('positionOverTime.json')
+df2 = pd.read_json('errorsOverDelta.json')
+
+delta = df2['delta']
+errorVerlet = df2['errorVerlet']
+errorBeeman = df2['errorBeeman']
+errorGCP = df2['errorGCP']
+
+pAnalytical = df['pAnalytical']
+pBeeman = df['pBeeman']
+pGCP = df['pGCP']
+pVerlet = df['pVerlet']
+x = df['time']'''
+
+QvsT()
+parcticlesvsT()
